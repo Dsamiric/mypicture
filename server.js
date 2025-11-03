9b83e6703c7d3832dd6903866ebf04c520dc48ed
Dsamiric <86196791+Dsamiric@users.noreply.github.com>
2025-11-03 21:18:02 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,55 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    // ako korisnik ide na /, otvori index.html
+    let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
+
+    // odredi content-type ovisno o ekstenziji
+    const ext = path.extname(filePath);
+    let contentType = 'text/html';
+    switch (ext) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+        case '.jpeg':
+            contentType = 'image/jpeg';
+            break;
+        case '.svg':
+            contentType = 'image/svg+xml';
+            break;
+    }
+
+    // čitaj datoteku i pošalji
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                res.writeHead(404, { 'Content-Type': 'text/plain' });
+                res.end('File not found');
+            } else {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Server error');
+            }
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(data);
+        }
+    });
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
+
